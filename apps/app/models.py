b170d797e099260b2853448abc251a4194838f50
Fernando Chavezgit config --global user.email you@yourdomain.example.c <fernandochajim@gmail.com>
2021-10-19 00:18:57 -0500

    actualizaciones varias

--- apps/app/models.py
+++ apps/app/models.py
@@ -1,5 +1,10 @@
 from django.db import models
 from django.contrib.auth.models import User
+from django.db.models.fields import FloatField
 
 # Create your models here.
-
+class Producto(models.Model):
+    id_producto = models.CharField(max_length=14, primary_key=True)
+    nom = models.CharField(max_length=25)
+    precio_venta = FloatField(max_length=25)
+    existencia = models.IntegerField()
