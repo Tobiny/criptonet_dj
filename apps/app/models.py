a05fd174a2cc990a7a68963033e26bd61d506456
Fernando Chavezgit config --global user.email you@yourdomain.example.c <fernandochajim@gmail.com>
2021-10-21 08:36:34 -0500

    modelos actualizados

--- apps/app/models.py
+++ apps/app/models.py
@@ -1,6 +1,5 @@
-from django.db import models
 from django.contrib.auth.models import User
-from django.db.models.fields import FloatField, DecimalField, BigAutoField
+from django.db import models
 from djmoney.models.fields import MoneyField
 
 
@@ -12,10 +11,10 @@ class Empleado(models.Model):
 
 
 class Producto(models.Model):
-    id_producto = models.CharField(max_length=14, primary_key=True)
+    id_producto = models.CharField(max_length=36, primary_key=True)
     nom = models.CharField(max_length=120)
-    precio_venta = MoneyField(max_length=18, decimal_places=2, max_digits=16)
-    cantidad_existente = models.PositiveIntegerField()
+    precio_venta = MoneyField(max_length=18, decimal_places=2, max_digits=16, help_text="Ingrese el precio del producto")
+    cantidad_existente = models.PositiveIntegerField(help_text="Ingrese la cantidad de producto")
 
 
 class Servicio(models.Model):
@@ -27,7 +26,7 @@ class Servicio(models.Model):
 class Mantenimiento(models.Model):
     id_manten = models.AutoField(primary_key=True)
     fecha = models.DateField()
-    observaciones = models.TextField()
+    observaciones = models.TextField(help_text="Ingrese las observaciones del mantenimiento")
     tipo_minero = models.CharField(max_length=20, null=True)
     total = MoneyField(max_length=18, decimal_places=2, max_digits=16)
 
@@ -40,9 +39,9 @@ class DetalleMatenimiento(models.Model):
 
 
 class DetalleVenta(models.Model):
-    id_producto = models.ForeignKey('Producto', on_delete=models.SET_NULL, null=True)
-    id_venta = models.ForeignKey('Venta', on_delete=models.CASCADE, null=True)
-    id_manten = models.ForeignKey('Mantenimiento', on_delete=models.SET_NULL, null=True)
+    id_producto = models.ForeignKey('Producto', on_delete=models.SET_NULL, null=True, blank=True)
+    id_venta = models.ForeignKey('Venta', on_delete=models.CASCADE, null=True, blank=True)
+    id_manten = models.ForeignKey('Mantenimiento', on_delete=models.SET_NULL, null=True, blank=True)
     cantidad = models.IntegerField()
     subtotal = MoneyField(max_length=18, decimal_places=2, max_digits=16)
 
@@ -52,14 +51,14 @@ class Cliente(models.Model):
     id_empleado = models.ForeignKey('Empleado', on_delete=models.CASCADE)
     nom = models.CharField(max_length=50, help_text="Ingrese el nombre del cliente")
     dom = models.CharField(max_length=100, help_text="Ingrese el domicilio del cliente")
-    mail = models.EmailField(max_length=254)
-    rfc = models.CharField(max_length=13, help_text='12-13 caracteres<a href="https://www.sat.gob.mx/consultas/44083'
+    mail = models.EmailField(max_length=254, help_text="Ingrese el correo del cliente")
+    rfc = models.CharField(max_length=13, help_text='12-13 caracteres <a href="https://www.sat.gob.mx/consultas/44083'
                                                     '/consulta-tu-informacion-fiscal '
-                                                    '">consulta tu RFC</a>')
+                                                    '"> consulta tu RFC</a>')
+
+
 class Venta(models.Model):
     id_venta = models.AutoField(primary_key=True)
     id_cliente = models.ForeignKey('Cliente', on_delete=models.CASCADE)
     total = MoneyField(max_length=18, decimal_places=2, max_digits=16)
     fecha = models.DateField()
-
-
