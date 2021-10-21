b8a0e40642b2a27754e086a68ae1fcbd370fe37e
Fernando Chavezgit config --global user.email you@yourdomain.example.c <fernandochajim@gmail.com>
2021-10-20 22:57:10 -0500

    Actualización modelos

--- apps/app/models.py
+++ apps/app/models.py
@@ -5,6 +5,12 @@ from djmoney.models.fields import MoneyField
 
 
 # Create your models here.
+class Empleado(models.Model):
+    id_empleado = models.AutoField(primary_key=True)
+    nom = models.CharField(max_length=50, help_text="Ingrese el nombre del empleado")
+    rfc = models.CharField(max_length=50, help_text="Ingrese el rfc del empleado")
+
+
 class Producto(models.Model):
     id_producto = models.CharField(max_length=14, primary_key=True)
     nom = models.CharField(max_length=120)
@@ -16,13 +22,6 @@ class Servicio(models.Model):
     id_servicio = models.AutoField(primary_key=True)
     precio = MoneyField(max_length=18, decimal_places=2, max_digits=16)
     nom = models.CharField(max_length=30)
-
-
-class Venta(models.Model):
-    id_venta = models.AutoField(primary_key=True)
-    id_cliente = models.ForeignKey('Venta', on_delete=models.CASCADE)
-    total = MoneyField(max_length=18, decimal_places=2, max_digits=16)
-    fecha = models.DateField()
 
 
 class Mantenimiento(models.Model):
@@ -43,7 +42,7 @@ class DetalleMatenimiento(models.Model):
 class DetalleVenta(models.Model):
     id_producto = models.ForeignKey('Producto', on_delete=models.SET_NULL, null=True)
     id_venta = models.ForeignKey('Venta', on_delete=models.CASCADE, null=True)
-    id_mant = models.ForeignKey('Mantenimiento', on_delete=models.SET_NULL, null=True)
+    id_manten = models.ForeignKey('Mantenimiento', on_delete=models.SET_NULL, null=True)
     cantidad = models.IntegerField()
     subtotal = MoneyField(max_length=18, decimal_places=2, max_digits=16)
 
@@ -57,9 +56,10 @@ class Cliente(models.Model):
     rfc = models.CharField(max_length=13, help_text='12-13 caracteres<a href="https://www.sat.gob.mx/consultas/44083'
                                                     '/consulta-tu-informacion-fiscal '
                                                     '">consulta tu RFC</a>')
+class Venta(models.Model):
+    id_venta = models.AutoField(primary_key=True)
+    id_cliente = models.ForeignKey('Cliente', on_delete=models.CASCADE)
+    total = MoneyField(max_length=18, decimal_places=2, max_digits=16)
+    fecha = models.DateField()
 
 
-class Empleado(models.Model):
-    id_empleado = models.AutoField(primary_key=True)
-    nom = models.CharField(max_length=50, help_text="Ingrese el nombre del empleado")
-    rfc = models.CharField(max_length=50, help_text="Ingrese el rfc del empleado")
