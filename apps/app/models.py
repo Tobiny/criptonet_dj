2bea861c0da7afac0c4396ecebbeea65196c8c35
Fernando Chavez <fernandochajim@gmail.com>
2022-11-17 19:46:25 -0600

    Update models.py

--- apps/app/models.py
+++ apps/app/models.py
@@ -292,39 +292,6 @@ class Producto(models.Model):
         super(Producto, self).save(*args, **kwargs)
 
 
-class DetalleProducto(models.Model):
-    # Related Fields
-    producto = models.ForeignKey(Producto, blank=True, null=True, on_delete=models.CASCADE)
-    recibo = models.ForeignKey(Recibo, on_delete=models.CASCADE, blank=True, null=True)
-    cantidad = models.FloatField(null=False)
-    subtotal = MoneyField(max_length=30, decimal_places=3, max_digits=27,
-                          default_currency='MXN', default=0)
-    preciototal = MoneyField(max_length=30, decimal_places=3, max_digits=27,
-                             default_currency='MXN', default=0)
-    # Utility fields
-    uniqueId = models.CharField(null=True, blank=True, max_length=100)
-    slug = models.SlugField(max_length=500, unique=True, blank=True, null=True)
-    date_created = models.DateTimeField(blank=True, null=True)
-    last_updated = models.DateTimeField(blank=True, null=True)
-    creacion = models.IntegerField(blank=True, null=True)
-
-    def __str__(self):
-        return '{}'.format(self.uniqueId)
-
-    def get_absolute_url(self):
-        return reverse('venta-detail', kwargs={'slug': self.slug})
-
-    def save(self, *args, **kwargs):
-        if self.date_created is None:
-            self.date_created = timezone.localtime(timezone.now())
-        if self.uniqueId is None:
-            self.uniqueId = str(uuid4()).split('-')[4]
-            self.slug = slugify('{} {}'.format(self.producto.modelo, self.uniqueId))
-
-        self.slug = slugify('{} {}'.format(self.producto.modelo, self.uniqueId))
-        self.last_updated = timezone.localtime(timezone.now())
-        super(DetalleProducto, self).save(*args, **kwargs)
-
 
 class VenderProducto(models.Model):
     billno = models.ForeignKey(ReciboVenta, on_delete=models.CASCADE, related_name='salebillno')
